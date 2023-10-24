2cf51e2bebf9c675ca161c7d0a08d71b80028f87
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2023-10-24 23:19:10 +0800

    Added Edit Profile Text Details

--- src/app/_components/profile/edit-profile-modal.tsx
+++ src/app/_components/profile/edit-profile-modal.tsx
@@ -1,9 +1,6 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-import { useState, ChangeEvent } from "react";
-
-import { api } from "~/trpc/react";
+import { ChangeEvent } from "react";
 
 interface ProfileData {
     firstName: string;
@@ -40,7 +37,6 @@ const changeProfilePhoto = (event: ChangeEvent) => {
   }
 }
 
-
 export function EditProfileModal({ onClose, data }: { onClose: () => void, data: ProfileData }) {
     return (
       <div
@@ -54,7 +50,7 @@ export function EditProfileModal({ onClose, data }: { onClose: () => void, data:
           {/* Modal content */}
           <div className="relative">
             {/* Modal header */}
-            <div className="flex items-start justify-between border-b rounded-t mb-3">
+            <div className="flex items-start justify-between border-b rounded-t">
               <h3 className="p-4 text-xl font-semibold text-gray-900">Edit Profile</h3>
               <button
                 onClick={onClose}
@@ -82,21 +78,65 @@ export function EditProfileModal({ onClose, data }: { onClose: () => void, data:
             </div>
             {/* Modal body */}
             <div className="m-auto">
-              <div className="h-96 overflow-y-auto">
+              <div className="">
                 {/* TODO: Edit Profile Content */}
                 <form method="PATCH" id="edit-profile-form">
-                  <div className="cursor-pointer absolute w-full flex justify-center group">
-                    <input onChange={changeCover} className="cursor-pointer z-20 opacity-0 w-full h-40 rounded-lg absolute" id="coverPhoto" type="file" name="coverPhoto" accept="image/*"/>
-                    <img id="newCoverPhoto" className="cursor-pointer w-full h-40 rounded-lg z-0 object-cover" src={data.banner} alt="" />
-                    <div className="text-sm text-white fixed mx-auto z-10 self-center opacity-0 group-hover:opacity-100">Choose New</div>
-                    <div className="absolute w-full h-40 rounded-lg bg-black opacity-0 group-hover:opacity-50"></div>
+                  <div> {/* This is to let the absolute position of cover + profile photos to anchor to this element */}
+                    <br></br>
                   </div>
+                  <div className="">
+                    <div className="cursor-pointer absolute w-full flex justify-center group">
+                      <input onChange={changeCover} className="cursor-pointer z-20 opacity-0 w-full h-40 rounded-lg absolute" id="coverPhoto" type="file" name="coverPhoto" accept="image/*"/>
+                      <img id="newCoverPhoto" className="cursor-pointer w-full h-40 rounded-lg z-0 object-cover" src={data.banner} alt="" />
+                      <div className="text-sm text-white fixed mx-auto z-10 self-center opacity-0 group-hover:opacity-100">Choose New</div>
+                      <div className="absolute w-full h-40 rounded-lg bg-black opacity-0 group-hover:opacity-70"></div>
+                    </div>
 
-                  <div className="cursor-pointer flex justify-start ml-4 md:ml-7 mt-16 w-32 h-32 md:h-36 md:w-36 rounded-full absolute group bg-green-500">
-                    <input onChange={changeProfilePhoto} className="cursor-pointer z-40 opacity-0 absolute w-32 h-32 md:h-36 md:w-36 rounded-full" id="profilePhoto" type="file" name="profilePhoto" accept="image/*"/>
-                    <div className="text-sm text-white mx-auto z-30 self-center opacity-0 group-hover:opacity-100">Choose New</div>
-                    <div className="border-4 border-white-1000 absolute z-20 w-32 h-32 md:h-36 md:w-36 rounded-full bg-black opacity-0 group-hover:opacity-50"></div>
-                    <img id="newProfilePhoto" className="cursor-pointer justify-start w-32 h-32 md:h-36 md:w-36 rounded-full absolute object-cover group-hover:z-0 border-4 border-white-1000 transform md:transform-none" src={data.profilePic} alt="" />
+                    <div className="cursor-pointer flex justify-start ml-2 md:ml-4 mt-16 w-32 h-32 md:h-36 md:w-36 rounded-full absolute group">
+                      <input onChange={changeProfilePhoto} className="cursor-pointer z-40 opacity-0 absolute w-32 h-32 md:h-36 md:w-36 rounded-full" id="profilePhoto" type="file" name="profilePhoto" accept="image/*"/>
+                      <div className="text-sm text-white mx-auto z-30 self-center opacity-0 group-hover:opacity-100">Choose New</div>
+                      <div className="border-4 border-white-1000 absolute z-20 w-32 h-32 md:h-36 md:w-36 rounded-full bg-black opacity-0 group-hover:opacity-70"></div>
+                      <img id="newProfilePhoto" className="cursor-pointer justify-start w-32 h-32 md:h-36 md:w-36 rounded-full absolute object-cover group-hover:z-0 border-4 border-white-1000 transform md:transform-none" src={data.profilePic} alt="" />
+                    </div>
+                  </div>
+                  
+                  
+                  {/* Text Edits */}
+                  <div className="mt-52 h-64 overflow-y-auto p-2">
+                    <div className="">
+                      <label htmlFor="firstName" className="pl-2 text-sm text-stone-500">First Name</label>
+                      <input id="firstName" name="firstName" className="border-2 border-stone-200 rounded p-2 w-full justify-center" placeholder="Enter your first name." value={data.firstName} type="text"></input>
+                    </div>
+                    <div className="mt-2">
+                      <label htmlFor="lastName" className="pl-2 text-sm text-stone-500">Last Name</label>
+                      <input id="lastName" name="lastName" className="border-2 border-stone-200 rounded p-2 w-full justify-center" placeholder="Enter your last name." value={data.lastName} type="text"></input>
+                    </div>
+                    <div className="mt-2">
+                      <label htmlFor="bio" className="pl-2 text-sm text-stone-500">Bio</label><br></br>
+                      <textarea className="border-2 border-stone-200 rounded p-2 w-full justify-center" name="bio" id="bio" rows={5} placeholder="Write something about yourself." value={data.bio}></textarea>
+                    </div>
+                    <div className="mt-1">
+                      <label htmlFor="location" className="pl-2 text-sm text-stone-500">Location</label>
+                      <input id="location" name="location" className="border-2 border-stone-200 rounded p-2 w-full justify-center" placeholder="Enter your location." value={data.location} type="text"></input>
+                    </div>
+                    <div className="mt-2">
+                      <label htmlFor="flavors" className="pl-2 text-sm text-stone-500">Flavors</label><br></br>
+                      <div className="flavors pl-2 ml-1" id="flavors">
+                        {/* TODO: Get list of flavors from database ? */}
+                        <input type="checkbox" id="sweet" name="sweet" value="sweet"/>
+                        <label htmlFor="sweet"> sweet </label><br></br>
+                        <input type="checkbox" id="spicy" name="spicy" value="spicy"/>
+                        <label htmlFor="spicy"> spicy </label><br></br>
+                        <input type="checkbox" id="umami" name="umami" value="umami"/>
+                        <label htmlFor="umami"> umami </label><br></br>
+                        <input type="checkbox" id="bitter" name="bitter" value="bitter"/>
+                        <label htmlFor="bitter"> bitter </label><br></br>
+                        <input type="checkbox" id="sour" name="sour" value="sour"/>
+                        <label htmlFor="sour"> sour </label><br></br>
+                      </div>
+                      
+                    </div>
+                    
                   </div>
                 </form>
                 
