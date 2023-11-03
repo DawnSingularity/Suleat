685d699d709d3c32625ea7a044b4c109bbca2fff
Harvey Chua <21309311+3liteking148@users.noreply.github.com>
2023-11-03 21:47:19 +0800

    Profile: also hide self-follow button in followers/following list

--- src/app/_components/profile/profile-user-list.tsx
+++ src/app/_components/profile/profile-user-list.tsx
@@ -8,8 +8,8 @@ import { useQueryClient } from "@tanstack/react-query";
 export default function UserList({ follower }: { follower: User }) {
   const queryClient = useQueryClient()
   
-  const username = api.profile.getCurrentUser.useQuery().data ?? ""
-  const followCheckQuery = {follower: username, following: follower.userName}
+  const loggedInUsername = api.profile.getCurrentUser.useQuery().data ?? ""
+  const followCheckQuery = {follower: loggedInUsername, following: follower.userName}
   const isFollowing = api.profile.isFollowing.useQuery(followCheckQuery).data
 
   const followMutation = api.profile.updateFollowState.useMutation()
@@ -23,6 +23,18 @@ export default function UserList({ follower }: { follower: User }) {
     })
   };
 
+  let followButton;
+  if(loggedInUsername !== follower.userName) {
+    followButton = (
+      <button
+      onClick={handleFollowToggle}
+      className={`w-[86px] px-4 py-1 rounded-lg text-xs font-bold text-${isFollowing ? 'blue-600' : 'white'}  ${isFollowing ? 'bg-gray-200' : 'bg-blue-600'}`}
+      >
+        {isFollowing ? "Unfollow" : "Follow"}
+      </button>
+    )
+  }
+
   return (
     <div className={`flex flex-row w-full gap-3 ${isFollowing ? 'bg-white border-blue-600' : 'bg-white'}`}>
       <div className="w-12">
@@ -35,12 +47,7 @@ export default function UserList({ follower }: { follower: User }) {
         <p className="text-xs font-font-medium color-gray">@{follower.userName}</p>
       </div>
       <div className="w-12">
-        <button
-          onClick={handleFollowToggle}
-          className={`w-[86px] px-4 py-1 rounded-lg text-xs font-bold text-${isFollowing ? 'blue-600' : 'white'}  ${isFollowing ? 'bg-gray-200' : 'bg-blue-600'}`}
-        >
-          {isFollowing ? "Unfollow" : "Follow"}
-        </button>
+        { followButton }
       </div>
     </div>
   );
