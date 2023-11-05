87dc2b4ac72ab16dfc6e0659350cd06c56f6d765
Harvey Chua <21309311+3liteking148@users.noreply.github.com>
2023-11-05 19:09:23 +0800

    Opt-in S3 upload support

--- src/app/api/content/[filename]/route.ts
+++ src/app/api/content/[filename]/route.ts
@@ -1,22 +1,39 @@
+import { GetObjectCommand } from '@aws-sdk/client-s3';
 import { fileTypeFromBuffer } from 'file-type';
 import { readFile } from 'fs/promises'
 import { NextRequest } from "next/server";
+import { s3 } from "~/s3"
 
 export async function GET(req: NextRequest,
                            { params }: { params: { filename: String }}) {
 
     const filename = params.filename
-    const path = "./uploads/" + filename
-    const file = await readFile(path)
-    
-    const type = await fileTypeFromBuffer(file.buffer)
 
-    if(file != null && type != null) {
-        const response = new Response(file.buffer)
-        response.headers.set("content-type", type.mime)
-        return response
-    } else {
-        return new Response(null, {status: 404})
+    let file : Buffer | Uint8Array | undefined
+    if(process.env.S3_BUCKET_NAME != null) {
+        const response = await s3.send(new GetObjectCommand({
+            Bucket: process.env.S3_BUCKET_NAME,
+            Key: filename.toString(),
+        }))
+        const byteArray = await response.Body?.transformToByteArray()
+        file = byteArray
     }
+    
+    // fallback to local
+    if(file == null) {
+        const path = "./uploads/" + filename
+        file = await readFile(path)
+    }
+    
+    if (file != null) {
+        const type = await fileTypeFromBuffer(file.buffer)
 
+        if(file != null && type != null) {
+            const response = new Response(file.buffer)
+            response.headers.set("content-type", type.mime)
+            return response
+        } else {
+            return new Response(null, {status: 404})
+        }
+    }
 }
