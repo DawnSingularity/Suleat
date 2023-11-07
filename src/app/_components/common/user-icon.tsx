c2512f33a0691fea620e560687f6fba2e0fba409
Harvey Chua <21309311+3liteking148@users.noreply.github.com>
2023-11-07 22:42:12 +0800

    Components: allow adding classes to UserIcon

--- src/app/_components/common/user-icon.tsx
+++ src/app/_components/common/user-icon.tsx
@@ -2,10 +2,10 @@
 
 import { User } from "prisma/prisma-client"
 
-export function UserIcon({ user, width }: { user: User, width: number | string }) {
+export function UserIcon({ user, width, className = "" }: { user: User, width: number | string, className?: string }) {
     return (
         <a href={`/profile/${user.userName}`}>
-                    <img className={`rounded-full h-${width} w-${width}`} src={user.pfpURL} alt="" />
+                    <img className={`rounded-full h-${width} w-${width} ${className}`} src={user.pfpURL} alt="" />
         </a>
     )
 }
