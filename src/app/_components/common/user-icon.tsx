21fc977761fabcde0380285b4220e6a067e4b1dc
Harvey Chua <21309311+3liteking148@users.noreply.github.com>
2023-11-10 19:37:46 +0800

    UserIcon: allow configuring clickability

--- src/app/_components/common/user-icon.tsx
+++ src/app/_components/common/user-icon.tsx
@@ -2,11 +2,13 @@
 
 import { User } from "prisma/prisma-client"
 
-export function UserIcon({ user, width, className = "" }: { user: User, width: number | string, className?: string }) {
-    return (
-        <a href={`/profile/${user.userName}`}>
-                    <img className={`rounded-full h-${width} w-${width} ${className}`} src={user.pfpURL} alt="" />
-        </a>
-    )
+export function UserIcon({ user, width, className = "", clickable = true }: { user: User, width: number | string, className?: string, clickable? : boolean }) {
+    const icon = <img className={`rounded-full h-${width} w-${width} ${className}`} src={user.pfpURL} alt="" />
+
+    if(clickable) {
+        return <a href={`/profile/${user.userName}`}> { icon } </a>
+    } else {
+        return icon
+    }
 }
   
