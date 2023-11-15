21ed81c95d59f014708bd4d8fb9fec7d40d709f7
Patrick James Marcellana <patrick_james_marcellana@dlsu.edu.ph>
2023-11-15 20:57:50 +0800

    add nav bar to post page

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -3,14 +3,16 @@
 import { api } from "~/trpc/react";
 import { PostView } from "~/app/_components/postview";
 import { LoadingPage } from "~/app/_components/loading";
+import { Navbar } from "~/app/_components/common/navbar"
 
 export default function Post({ params }: { params: { id: string } }) {
   const { data, isLoading } = api.post.getPostById.useQuery({ id: params.id });
 
-  if(!data) return <div>Something went wrong</div>; 
+  if(!data) return <div className="mx-auto">Something went wrong</div>; 
   if(isLoading) return <LoadingPage/>;
   return (
       <main>
+        <Navbar />
         <PostView {...data}/>
       </main>
     );
