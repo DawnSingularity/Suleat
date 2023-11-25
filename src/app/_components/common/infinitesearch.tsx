aaf0507939709644493717274407db67644eece3
Harvey Chua <21309311+3liteking148@users.noreply.github.com>
2023-11-25 21:56:13 +0800

    Infinite search: combine in one file

--- src/app/_components/common/infinitesearch.tsx
+++ src/app/_components/common/infinitesearch.tsx
@@ -1,14 +1,16 @@
 "use client";
 
 import { api } from "~/trpc/react";
+import { RouterOutputs } from "~/trpc/shared";
 import { useInView } from "react-intersection-observer";
 import { getQueryKey } from "@trpc/react-query";
 import { Fragment, useEffect } from "react";
 import { PostComponent } from "./post_v1";
+import { UserComponent } from "./user-search";
 import { useSearchParams } from 'next/navigation'
 import { LoadingSpinner } from "../loading";
 
-export function InfiniteSearch() {
+export function InfiniteSearch({ category } : { category: string }) {
     const searchParams = useSearchParams()
     const searchKey = searchParams.get('search')
     const loadingScreen = (<div className="h-full flex items-center justify-center"><LoadingSpinner size={40}/></div>)
@@ -19,9 +21,19 @@ export function InfiniteSearch() {
     if(searchKey !== null) {
         key = searchKey
     }
+    
+    const getRPC = () => {
+        switch(category) {
+            case "users":
+                return api.profile.getSearchUsers
+            case "posts":
+            default:
+                return api.post.getSearchPosts
+        }
+    }
 
     let receivedCount = 0
-    const infiniteQuery = api.post.getSearchPosts.useInfiniteQuery(
+    const infiniteQuery = getRPC().useInfiniteQuery(
         {
             limit: 5,
             searchKey: key
@@ -49,7 +61,7 @@ export function InfiniteSearch() {
         receivedCount += page.length
     }
 
-    console.log("Obtained Data: " + infiniteQuery)
+    console.log("Obtained data count: " + receivedCount)
     useEffect(() => {
         if(scrollMonitorInView && infiniteQuery.hasNextPage) {
             infiniteQuery.fetchNextPage()
@@ -58,26 +70,46 @@ export function InfiniteSearch() {
 
     // remove possible duplicate posts
     // note: there might be a better way for this
-    const renderedPosts = new Set()
-    const posts = infiniteQuery.data?.pages.map((page, index) => (
-    <Fragment key={index}>
-        {page.map((post) => {
-            if (!renderedPosts.has(post.id)) {
-                renderedPosts.add(post.id)
-                return <PostComponent post={post} />
-            } else {
-                // do nothing
-                return <></>
-            }
-        })}
-    </Fragment>
-  ))
-
-  return (<div>
-    {
-        infiniteQuery.isSuccess ? (receivedCount === 0 ? <div className="text-lg">No search results found.</div> : posts) : (infiniteQuery.isLoading ? loadingScreen : (<></>))
+    const renderedSet = new Set()
+    const renderPage = (page : unknown) => {
+        switch(category) {
+            case "users":
+                const users = page as RouterOutputs["profile"]["getSearchUsers"]
+                return users.map((user) => {
+                    if(!renderedSet.has(user.uuid)) {
+                        renderedSet.add(user.uuid)
+                        return <UserComponent user={user} />
+                    } else {
+                        // do nothing
+                        return <></>
+                    }
+                })
+            case "posts":
+            default:
+                const posts = page as RouterOutputs["post"]["getSearchPosts"]
+                return posts.map((post) => {
+                    if(!renderedSet.has(post.id)) {
+                        renderedSet.add(post.id)
+                        return <PostComponent post={post} />
+                    } else {
+                        // do nothing
+                        return <></>
+                    }
+                })
+        }
     }
     
-    <div ref={scrollMonitorRef}></div>
-  </div>)
+    const renderedResults = infiniteQuery.data?.pages.map((page : unknown, index) => (
+        <Fragment key={index}>
+            { renderPage(page) }
+        </Fragment>
+    ))
+
+    return (<div>
+        {
+            infiniteQuery.isSuccess ? (receivedCount === 0 ? <div className="text-lg">No search results found.</div> : renderedResults) : (infiniteQuery.isLoading ? loadingScreen : (<></>))
+        }
+        
+        <div ref={scrollMonitorRef}></div>
+      </div>)
 }
