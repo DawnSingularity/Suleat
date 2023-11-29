045b4d1ad21a3c6f7c974d416699066a556c722a
DawnSingularity <70613207+DawnSingularity@users.noreply.github.com>
2023-11-29 20:19:25 +0800

    added mouseeventhandler for user-icon

--- src/app/_components/common/user-icon.tsx
+++ src/app/_components/common/user-icon.tsx
@@ -1,9 +1,20 @@
 "use client";
 
 import { User } from "prisma/prisma-client"
-
-export function UserIcon({ onClick, user, width, className = "", clickable = true }: { onClick?: (x : boolean) => void, user: User, width: number | string, className?: string, clickable? : boolean }) {
-    const icon = <img onClick={onClick} className={`rounded-full h-${width} w-${width} ${className}`} src={user.pfpURL} alt="" />
+import { MouseEventHandler } from 'react';
+export function UserIcon({
+    onClick,
+    user,
+    width,
+    className = "",
+    clickable = true
+  }: {
+    onClick?: MouseEventHandler<HTMLImageElement>,
+    user: User,
+    width: number | string,
+    className?: string,
+    clickable?: boolean
+  }) {    const icon = <img onClick={onClick} className={`rounded-full h-${width} w-${width} ${className}`} src={user.pfpURL} alt="" />
 
     if(clickable) {
         return <a href={`/profile/${user.userName}`}> { icon } </a>
